bd21320afa443a5e938c53ade5396e9da0e18ff9
Robert Quintana <robertquintanatx@gmail.com>
2020-06-19 08:00:32 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,11 +1,16 @@
 require 'pry'
 require 'json' 
 require 'net/http'
+require 'open-uri'
 
 class GetRequester
   
   def initialize(link)
-    
-    
+    u = URI.parse(link)
+    response = Net::HTTP.get_response(u)
+  end 
+  
+  def parse_json
+    JSON.parse(response.body)
   end 
 end 
